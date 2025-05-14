a3aa178b74ecf9fccab812208d42693f4b1d1fb8
Robby94gtr <robertogramazio94@gmail.com>
2025-05-14 11:59:35 +0200

    [UPD] Created array of articles and linked bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import { useState } from 'react'
 
+const articoli =[
+  {
+    id: 1,
+    title: "Titolo 1"
+  },
+  {
+    id: 2,
+    title: "Titolo 2"
+  },
+  {
+    id: 3,
+    title: "Titolo 3"
+  },
+  {
+    id: 4,
+    title: "Titolo 4"
+  },
+  {
+    id: 5,
+    title: "Titolo 5"
+  },
+]
 
 function App() {
 
